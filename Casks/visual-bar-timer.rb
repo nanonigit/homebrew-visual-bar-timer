800d2dcd9cfd1105dcbd02ac4ed74851b50d6bf7
nanonigit <nanoni@gmail.com>
2026-08-27 07:48:13 +0900

    feat: initial homebrew cask for VisualBarTimer

---
+++ Casks/visual-bar-timer.rb
@@ -0,0 +1,15 @@
+cask "visual-bar-timer" do
+  version "1.0.0"
+  sha256 "3b83525a16f793e8a46b5a9ce895f50206d4b5fdcb06545523972fddb635b55f"
+
+  url "https://github.com/nanonigit/VisualBarTimer/releases/download/v#{version}/VisualBarTimer.zip"
+  name "VisualBarTimer"
+  desc "Kingjim-style visual bar timer for macOS"
+  homepage "https://github.com/nanonigit/VisualBarTimer"
+
+  app "VisualBarTimer.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.naoki.VisualBarTimer.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew Tap for VisualBarTimer
+
+Official Homebrew tap for [VisualBarTimer](https://github.com/nanonigit/VisualBarTimer).
+
+## Installation
+
+```bash
+brew install --cask nanonigit/visual-bar-timer/visual-bar-timer
+```
+
+or
+
+```bash
+brew tap nanonigit/visual-bar-timer
+brew install --cask visual-bar-timer
+```
