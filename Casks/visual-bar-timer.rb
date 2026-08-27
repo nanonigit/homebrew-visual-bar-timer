f2dd4ba373a4555aae1658a2a781551783cfc9dd
nanonigit <nanoni@gmail.com>
2026-08-27 19:20:56 +0900

    bump: visual-bar-timer v1.2.1

--- Casks/visual-bar-timer.rb
+++ Casks/visual-bar-timer.rb
@@ -1,6 +1,6 @@
 cask "visual-bar-timer" do
-  version "1.2.0"
-  sha256 "012d25d9f1a2abf9f561e8151570d9af5f819adda6a6b2631b8c49f43c63b156"
+  version "1.2.1"
+  sha256 "99c2e128ac819428d8de2bb8031d017406755c4634f5a02c7a239df465d2ca47"
 
   url "https://github.com/nanonigit/VisualBarTimer/releases/download/v#{version}/VisualBarTimer.zip"
   name "VisualBarTimer"
